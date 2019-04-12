7ba81760805ac5309be0082faab45ac5815b1791
siavash <siavash.kavousi.sk@gmail.com>
2019-04-12 12:43:50 +0430

    feat(sample): add new samples

---
+++ samples/longtaskfunc/handler.js
@@ -0,0 +1,5 @@
+console.log('gonna wait for 5 seconds');
+
+setTimeout(() => {
+    console.log('callback is called');
+}, 5000);
--- samples/nodeinfo/main.js
+++ samples/nodeinfo/main.js
@@ -2,9 +2,9 @@
 
 let os = require('os');
 // const getStdin = require('get-stdin');
-//
+
 // getStdin().then(content => {
-//     console.log(content);
+    // console.log(content);
 //     console.log('----------------');
-    console.log(os.platform(), os.arch(), os.cpus(), os.uptime(), os.userInfo());
+console.log(os.platform(), os.arch(), os.cpus(), os.uptime(), os.userInfo());
 // });
