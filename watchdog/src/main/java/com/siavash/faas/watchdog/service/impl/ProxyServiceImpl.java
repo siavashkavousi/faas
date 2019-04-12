4e50cbc1f50b0f6adf3c46dcb46beb8f0bc3863e
siavash <siavash.kavousi.sk@gmail.com>
2019-04-12 11:15:12 +0430

    refactor(watchdog): minor changes

--- watchdog/src/main/java/com/siavash/faas/watchdog/service/impl/ProxyServiceImpl.java
+++ watchdog/src/main/java/com/siavash/faas/watchdog/service/impl/ProxyServiceImpl.java
@@ -4,9 +4,9 @@ import com.siavash.faas.watchdog.config.Configs;
 import com.siavash.faas.watchdog.service.ProxyService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
 
@@ -23,27 +23,28 @@ public class ProxyServiceImpl implements ProxyService {
 	@Override
 	public String exec(String request) throws Exception {
 		ProcessBuilder builder = new ProcessBuilder();
-		builder.command(Arrays.asList(configs.getFunctionCommand().split(" ")));
+		builder.command(configs.getFunctionCommand().split(" "));
 		builder.redirectErrorStream(true);
 		Process process = builder.start();
 
-		OutputStream stdin = process.getOutputStream();
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
-		writer.write(request);
-		writer.flush();
-		writer.close();
+		if (!StringUtils.isEmpty(request)) {
+			OutputStream stdin = process.getOutputStream();
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(stdin));
+			writer.write(request);
+			writer.flush();
+			writer.close();
+		}
 
 		StringBuilder output = new StringBuilder();
 		StreamGobbler streamGobbler = new StreamGobbler(process.getInputStream(), content -> output.append(content).append("\n"));
 		Executors.newSingleThreadExecutor().submit(streamGobbler);
 
 		int exitCode = process.waitFor();
-		if (exitCode == 0) {
-			return output.toString();
-		} else {
+		if (exitCode != 0) {
 			logger.error("process finished with status code: {}", exitCode);
-			return output.toString();
 		}
+
+		return output.toString();
 	}
 
 	private static class StreamGobbler implements Runnable {
